5c6c0b9e5b2e7507d9d3c116c015ac291e483e98
Doug Tolley <det7777@gmail.com>
2025-08-12 12:36:22 -0400

    Update fnSplitString.sql

--- fnSplitString.sql
+++ fnSplitString.sql
@@ -40,53 +40,54 @@ WITH SourceText AS
               OR @Delimiter IS NULL
               OR @Delimiter = N''
               OR LEN(@DelimitedString) = 0
-                THEN NULL
+                THEN NULL                                  -- empty input => no rows
             WHEN RIGHT(@DelimitedString, 1) = @Delimiter
                 THEN LEFT(@DelimitedString, LEN(@DelimitedString) - 1)  -- drop trailing delimiter
             ELSE @DelimitedString
         END
 )
 SELECT
-    Ordinal = CONVERT(INT, [key]) + 1,              -- 1-based position
-    Value   = CONVERT(NVARCHAR(MAX), [value])       -- preserve spaces like legacy
+    Ordinal = CONVERT(INT, [key]) + 1,                    -- 1-based position
+    Value   = CONVERT(NVARCHAR(MAX), [value])             -- preserve whitespace like legacy
 FROM SourceText
 CROSS APPLY OPENJSON(
     CASE
-        WHEN AdjustedString IS NULL THEN NULL
-        ELSE N'[''' + REPLACE(AdjustedString, @Delimiter, N''',''') + N''']'
+        WHEN AdjustedString IS NULL THEN N'[]'
+        ELSE N'["' + REPLACE(AdjustedString, @Delimiter, N'","') + N'"]'
     END
 );
-GO
+
 
 /* -----------------------------------------
    Repro again, but join on Ordinal.
    We STILL shuffle each side independently,
    yet the pairing remains correct.
    ----------------------------------------- */
-DECLARE @SelectedTickets2     NVARCHAR(MAX) = N'4897,4900';
-DECLARE @SelectedTicketAmts2  NVARCHAR(MAX) = N'62000.00,398000.00';
+DECLARE @SelectedTickets      VARCHAR(MAX) = '';
+DECLARE @SelectedTicketsAmts  VARCHAR(MAX) = '';
 
-WITH TicketParts AS
+SET @SelectedTickets     = '403089039,403089041,403089040';
+SET @SelectedTicketsAmts = '50000000.00,50000000.00,10584205.00';
+
+
+;WITH TicketParts AS
 (
     SELECT Ordinal, Ticket = TRY_CONVERT(INT, Value)
-    FROM dbo.fnSplitString(@SelectedTickets2, N',')
+    FROM dbo.fnSplitString(@SelectedTickets, N',')
 ),
 AmountParts AS
 (
     SELECT Ordinal, Amount = TRY_CONVERT(DECIMAL(18,2), Value)
-    FROM dbo.fnSplitString(@SelectedTicketAmts2, N',')
-),
-TicketPartsShuffled AS
-(
-    SELECT * FROM TicketParts ORDER BY CHECKSUM(NEWID())     -- simulate reordering
-),
-AmountPartsShuffled AS
-(
-    SELECT * FROM AmountParts ORDER BY CHECKSUM(NEWID())     -- simulate independent reordering
+    FROM dbo.fnSplitString(@SelectedTicketsAmts, N',')
 )
 SELECT
-    Result = CONCAT('Ticket ', TicketPartsShuffled.Ticket, ' => $', AmountPartsShuffled.Amount)
-FROM TicketPartsShuffled
-JOIN AmountPartsShuffled
-  ON AmountPartsShuffled.Ordinal = TicketPartsShuffled.Ordinal
-ORDER BY TicketPartsShuffled.Ordinal;                        -- preserves original sequence
+    TicketParts.Ordinal,
+    TicketParts.Ticket,
+    AmountParts.Amount
+FROM TicketParts
+JOIN AmountParts
+  ON TicketParts.Ordinal = AmountParts.Ordinal
+ORDER BY TicketParts.Ordinal;
+
+
+
